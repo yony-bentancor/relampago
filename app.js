1d61e6d11743e6900c4a214792e2849c8acfbc12
yony-bentancor <yonybentancor@gmail.com>
2026-04-28 21:49:26 -0300

    upd

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const padresRoutes = require("./routes/padres");
 
 const app = express();
 
-//connectDB();
+connectDB();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
