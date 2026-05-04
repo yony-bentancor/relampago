a6f203c04e0b52083e2da16f9d443e4009b4e6cf
yony-bentancor <yonybentancor@gmail.com>
2026-05-04 16:57:32 -0300

    upd

--- routes/public.js
+++ routes/public.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Inscripcion = require("../models/Inscripcion");
 
 // ===============================
 // MODELOS MONGOOSE DESACTIVADOS
